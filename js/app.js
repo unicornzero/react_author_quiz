c1feb1ad036a81a010aa85266313a32c7d513e80
Rylee Keys <rylee.k@gmail.com>
2015-06-15 17:37:25 -0600

    add data, images, and container divs

--- js/app.js
+++ js/app.js
@@ -3,22 +3,62 @@
   'use strict';
 
   var Quiz = React.createClass({
+    propTypes: {
+      books: React.PropTypes.array.isRequired
+    },
+    getInitialState: function () {
+      return {
+        author: this.props.data[0],
+        books: this.props.data[0].books
+      };
+    },
     render: function () {
       return <div>
-          {this.props.books.map(function(b) {
-            return <Book title={b} />;
-          })}
-        </div>;
+          <div className="row">
+            <div className="col-md-4">
+              <img src={this.state.author.imageUrl} className="authorimage col-md-3" />
+            </div>
+            <div className="col-md-7">
+              {this.state.books.map(function (b) {
+                return <Book title={b} />;
+              }, this)}
+            </div>
+            <div className="col-md-1"></div>
+          </div>
+        </div>
     }
   });
 
   var Book = React.createClass({
+    propTypes: {
+      title: React.PropTypes.string.isRequired
+    },
     render: function () {
       return <div><h4>{this.props.title}</h4></div>;
     }
   })
 
-  React.render(<Quiz books={['The Lord of The Rings', 'The Adventures of Tom Sawyer']} />, 
+  var data = [
+    {
+      name: 'Mark Twain',
+      imageUrl: 'images/authors/marktwain.jpg',
+      books: ['The Adventures of Huckleberry Finn']
+    },
+    {
+      name: 'Joseph Conrad',
+      imageUrl: 'images/authors/josephconrad.png',
+      books: ['Heart of Darkness']
+    },
+    {
+      name: 'J.K. Rowling',
+      imageUrl: 'images/authors/jkrowling.jpg',
+      imageSource: 'Wikimedia Commons',
+      imageAttribution: 'Daniel Ogren',
+      books: ['Harry Potter and the Sourcerers Stone', 'Harry Potter and the Chamber of Secrets']
+    }
+  ]
+
+  React.render(<Quiz data={data} />, 
     document.getElementById('app'));
 
 })();
