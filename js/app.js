9815a3d1636e47796539df6904cae80d6f570b98
Rylee Keys <rylee.k@gmail.com>
2015-06-15 12:50:24 -0600

    move data to props

--- js/app.js
+++ js/app.js
@@ -4,11 +4,21 @@
 
   var Quiz = React.createClass({
     render: function () {
-      return <div>test {this.props.data}</div>;
+      return <div>
+          {this.props.books.map(function(b) {
+            return <Book title={b} />;
+          })}
+        </div>;
     }
   });
 
-  React.render(<Quiz data={"foo"}/>, 
+  var Book = React.createClass({
+    render: function () {
+      return <div><h4>{this.props.title}</h4></div>;
+    }
+  })
+
+  React.render(<Quiz books={['The Lord of The Rings', 'The Adventures of Tom Sawyer']} />, 
     document.getElementById('app'));
 
 })();
