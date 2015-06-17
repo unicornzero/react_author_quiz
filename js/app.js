6aa597cceb10d9066d8aefbd2554a08d10b78f33
Rylee Keys <rylee.k@gmail.com>
2015-06-17 15:45:17 -0600

    make game interactive

--- js/app.js
+++ js/app.js
@@ -7,22 +7,42 @@
       books: React.PropTypes.array.isRequired
     },
     getInitialState: function () {
-      return this.props.data.selectGame();
+      return _.extend({
+        bgClass: 'neutral',
+        showContinue: false
+      }, this.props.data.selectGame());
+    },
+    handleBookSelected: function (title) {
+      var isCorrect = this.state.checkAnswer(title);
+      this.setState({
+        bgClass: isCorrect ? 'pass' : 'fail',
+        showContinue: isCorrect
+      });
+    },
+    handleContinue: function (title) {
+      this.setState(this.getInitialState());
     },
     render: function () {
-      return <div>
+      return (<div>
           <div className="row">
             <div className="col-md-4">
               <img src={this.state.author.imageUrl} className="authorimage col-md-3" />
             </div>
             <div className="col-md-7">
               {this.state.books.map(function (b) {
-                return <Book title={b} />;
+                return <Book onBookSelected={this.handleBookSelected} title={b} />;
               }, this)}
             </div>
-            <div className="col-md-1"></div>
+            <div className={"col-md-1 " + this.state.bgClass}></div>
           </div>
-        </div>
+          {this.state.showContinue? (
+            <div className="row">
+              <div className="col-md-12">
+                <input onClick={this.handleContinue} type="button" value="Next Quiz" class="btn "/>
+              </div>
+            </div>) : <span/>
+          }
+        </div>);
     }
   });
 
@@ -30,8 +50,13 @@
     propTypes: {
       title: React.PropTypes.string.isRequired
     },
+    handleClick: function () {
+      this.props.onBookSelected(this.props.title);
+    },
     render: function () {
-      return <div className="answer"><h4>{this.props.title}</h4></div>;
+      return  <div onClick={this.handleClick} className="answer">
+                <h4>{this.props.title}</h4>
+              </div>;
     }
   })
 
@@ -68,7 +93,13 @@
         return author.books.some(function (title) {
           return title === answer;
         });
-      })
+      }),
+
+      checkAnswer: function (title) {
+        return this.author.books.some(function (t) {
+          return t === title;
+        });
+      }
     };
   };
 
